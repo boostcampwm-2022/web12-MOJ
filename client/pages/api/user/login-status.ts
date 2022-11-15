f6a3a1c3f28ef5b4c5de80a461a9c1278a44239a
dolphinlmg <39158895+dolphinlmg@users.noreply.github.com>
2022-11-15 15:00:44 +0900

    fix: login status api 수정
    로그인시 userName 추가
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>

--- client/pages/api/user/login-status.ts
+++ client/pages/api/user/login-status.ts
@@ -1,9 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 function mockHandler(req: NextApiRequest, res: NextApiResponse) {
-  const status = ['logged in', 'logged out'];
+  const result = [
+    { status: 'logged in', userName: 'hyoseok' },
+    { status: 'logged out' },
+  ];
   const rnd = Math.floor(Math.random() * 2);
-  return res.status(rnd === 0 ? 200 : 401).json({ status: status[rnd] });
+
+  return res.status(rnd === 0 ? 200 : 401).json(result[rnd]);
 }
 
 export default mockHandler;
