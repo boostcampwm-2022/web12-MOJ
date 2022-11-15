a7c134c084a1e665ca2d304d6035105604924904
dolphinlmg <39158895+dolphinlmg@users.noreply.github.com>
2022-11-15 15:28:46 +0900

    1 공통 - 상단(GNB) #5 (#55)
    * feat: global css 적용
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * feat: GNB 컴포넌트 작성
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * chore: strict 모드 해제, eslint 수정
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * feat: Close #6
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * feat: user 로그인 상태 mock api 구현
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * feat: Close #15, #16
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * fix: login status api 수정
    
    로그인시 userName 추가
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * feat: Close #14
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * feat: GNB 적용
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,19 @@
+import { Global, css } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import GNB from '../components/GNB';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+          }
+        `}
+      />
+      <GNB />
+      <Component {...pageProps} />
+    </>
+  );
 }
