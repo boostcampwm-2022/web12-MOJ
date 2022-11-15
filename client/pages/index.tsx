a25cf3e5af048f128243f2516e918cf39badb0bc
HD152513 <qag331@naver.com>
2022-11-15 15:32:10 +0900

    feat: 2-5 처음 들어왔을 때는 전체 문제 목록을 보여준다. (#34)
    Co-authored-by: hyoseok <janghs0604@gmail.com>

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import { css } from '@emotion/react';
+import ProblemList from '../components/problemList';
+import axios from 'axios';
+import axiosInstance from '../axios';
+
+const style = {
+  container: css`
+    margin: 0 20%;
+  `,
+  title: css`
+    font-size: 32px;
+    font-weight: bold;
+  `,
+};
+
+interface ProblemSummary {
+  id: number;
+  title: string;
+  rate: number;
+}
+
+interface ProblemListResponseData {
+  pageCount: number;
+  currentPage: number;
+  problems: ProblemSummary[];
+}
 
 function Home() {
-  return <div>Hello World!</div>;
+  const [problems, setProblems] =
+    React.useState<ProblemListResponseData | null>(null);
+
+  React.useEffect(() => {
+    async function fetchProblemList() {
+      const { data } = await axiosInstance.get('/api/problem');
+      setProblems(data);
+    }
+    fetchProblemList();
+  }, []);
+
+  return (
+    <div css={style.container}>
+      <div css={style.title}>문제 목록</div>
+      {problems === null ? (
+        <div>로딩중</div>
+      ) : (
+        <ProblemList {...problems}></ProblemList>
+      )}
+    </div>
+  );
 }
 
 export default Home;
+
+//
+// 문제 [{ number: , title: , 비율:  }, { }];
+// 제출 [{ number: , id: , 문제: , 결과: , 시간: , 제출 시각 }]
+// 출제 [{ number: , title: , 날짜: }]; + 버튼들 UI 상에서만.
+
+// data []
+// colums : [ { name: "문제 번호", path: "number"}, { name: "제목", path: "title" } ]
+// style...
