b8fc4dc380e62944d0806bf4e45e1358bff340e7
dolphinlmg <39158895+dolphinlmg@users.noreply.github.com>
2022-11-15 16:13:44 +0900

    feat: Button 컴포넌트 작성
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>

--- client/components/GNB/UserInfo.tsx
+++ client/components/GNB/UserInfo.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import Button from '../common/Button';
 
 interface UserInfoProps {
   isLoggedIn: boolean;
@@ -11,21 +12,6 @@ const userInfoStyle = css`
   align-items: center;
 `;
 
-const btnStyle = css`
-  background-color: #6f74dd;
-  border-radius: 1rem;
-  padding: 5px 15px;
-  color: white;
-  font-size: 14px;
-  margin: 0 15px;
-  transition: all 0.2s;
-
-  &:hover {
-    cursor: pointer;
-    box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.2);
-  }
-`;
-
 const userNameStyle = css`
   color: white;
 `;
@@ -34,7 +20,7 @@ function UserInfo({ isLoggedIn, userName }: UserInfoProps) {
   return (
     <div css={userInfoStyle}>
       <span css={userNameStyle}>{isLoggedIn ? userName : ''}</span>
-      <div css={btnStyle}> {isLoggedIn ? '로그아웃' : '로그인'}</div>
+      <Button>{isLoggedIn ? '로그아웃' : '로그인'}</Button>
     </div>
   );
 }
