de3978c34664f437131acab470c26426de18381f
dolphinlmg <39158895+dolphinlmg@users.noreply.github.com>
2022-11-15 14:55:21 +0900

    feat: user 로그인 상태 mock api 구현
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>

---
+++ client/pages/api/user/login-status.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+function mockHandler(req: NextApiRequest, res: NextApiResponse) {
+  const status = ['logged in', 'logged out'];
+  const rnd = Math.floor(Math.random() * 2);
+  return res.status(rnd === 0 ? 200 : 401).json({ status: status[rnd] });
+}
+
+export default mockHandler;
