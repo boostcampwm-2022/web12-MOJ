dda7b35166e0d53bc6a2b5b60361061c327696f2
Minhui Park <qag331@naver.com>
2022-11-16 10:02:04 +0900

    2. 문제 목록 (#8) (#56)
    * feat: 2-1 head 에는 문제 번호, 제목, 정답 비율이 위치한다 (#24)
    
    Co-authored-by: hyoseok <janghs0604@gmail.com>
    
    * feat: 2-2 문제 리스트를 20개씩 보여준다. (#30)
    
    Co-authored-by: hyoseok <janghs0604@gmail.com>
    
    * feat: mock api 작성
    
    Co-authored-by: hyoseok <janghs0604@gmail.com>
    
    * feat: 2-5 처음 들어왔을 때는 전체 문제 목록을 보여준다. (#34)
    
    Co-authored-by: hyoseok <janghs0604@gmail.com>
    
    * feat: 2-4 리스트 행 클릭시 문제 상세 페이지로 이동한다. (#33)
    
    Co-authored-by: hyoseok <janghs0604@gmail.com>
    
    * chore: list style 수정
    
    Co-authored-by: hyoseok <janghs0604@gmail.com>
    
    * feat: 2-6 페이지네이션이 리스트 하단에 위치한다. (#35)
    
    Co-authored-by: hyoseok <janghs0604@gmail.com>
    
    Co-authored-by: hyoseok <janghs0604@gmail.com>

---
+++ client/components/paginator.tsx
@@ -0,0 +1,136 @@
+import { css } from '@emotion/react';
+import Next from '../public/next.svg';
+import Image from 'next/image';
+import Prev from '../public/prev.svg';
+import { Fragment } from 'react';
+import Link from 'next/link';
+
+interface PaginatorProps {
+  pageCount: number;
+  currentPage: number;
+  href: (page: number) => string;
+}
+
+const style = {
+  container: css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 16px;
+    color: #636971;
+    padding: 15px;
+  `,
+  page: css`
+    padding: 4px 8px;
+    border-radius: 2px;
+    :hover {
+      cursor: pointer;
+    }
+  `,
+  'page-active': css`
+    background-color: #f6f4f3;
+  `,
+  active: css`
+    color: #000000;
+    font-weight: 500;
+    :hover {
+      cursor: pointer;
+    }
+  `,
+  buttonContainer: css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 8px;
+  `,
+  unset: css`
+    all: unset;
+  `,
+  disabled: css`
+    pointer-events: none;
+  `,
+};
+
+function Paginator({ pageCount, currentPage, href }: PaginatorProps) {
+  let start = Math.max(1, currentPage - 2);
+  let end = Math.min(currentPage + 2, pageCount);
+  const isPrev = start > 1;
+  const isNext = end < pageCount;
+
+  if (end - start + 1 < Math.min(5, pageCount)) {
+    const need = Math.min(5, pageCount) - (end - start + 1);
+    if (start == 1) end += need;
+    else start -= need;
+  }
+
+  const pages = Array.from({ length: end - start + 1 }, (_, i) => start + i);
+
+  return (
+    <div css={style.container}>
+      <Link
+        css={[style.unset, isPrev ? undefined : style.disabled]}
+        href={isPrev ? href(start - 1) : '#'}
+      >
+        <div
+          css={[style.buttonContainer, isPrev ? style.active : style.disabled]}
+        >
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12.5 5L7.5 10L12.5 15"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <div>{'Prev'}</div>
+        </div>
+      </Link>
+      {pages.map((page) => (
+        <Link href={href(page)} key={page} css={style.unset}>
+          <div
+            css={[
+              style.page,
+              ...(page === currentPage
+                ? [style.active, style['page-active']]
+                : []),
+            ]}
+          >
+            {page}
+          </div>
+        </Link>
+      ))}
+      <Link
+        css={[style.unset, isNext ? undefined : style.disabled]}
+        href={isNext ? href(end + 1) : '#'}
+      >
+        <div css={[style.buttonContainer, isNext ? style.active : undefined]}>
+          <div>{'Next'}</div>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.5 15L12.5 10L7.5 5"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default Paginator;
---
+++ client/pages/api/problem.tsx
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'GET') {
+    const { page } = req.query;
+    let _page = 1;
+
+    if (!page) _page = 1;
+    else if (Array.isArray(page)) _page = 1;
+    else _page = +page;
+
+    const problems = Array.from({ length: 20 }, (_, i) => {
+      return {
+        id: (_page - 1) * 20 + i + 1,
+        title: `${(_page - 1) * 20 + i + 1}번째 문제 - 알고리즘 망령 효석이`,
+        rate: 50.5 + i,
+      };
+    });
+
+    const pageCount = 8;
+    const currentPage = _page;
+
+    res.status(200).json({
+      pageCount: pageCount,
+      problems: problems,
+      currentPage: currentPage,
+    });
+  } else {
+    res.status(404);
+  }
+}
