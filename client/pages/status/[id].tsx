d23a408c9a84aae23e961b66a1b9abe750950d4a
joo-prog <1114nhj@naver.com>
2022-11-16 16:43:37 +0900

    feat: close #17
    풀이 status list 추가
    
    Co-authored-by: dolphinlmg <dolphinlmg@users.noreply.github.com>

--- client/pages/status/[id].tsx
+++ client/pages/status/[id].tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import Button from '../../components/common/Button';
 import { css } from '@emotion/react';
 import Editor from '@monaco-editor/react';
+import StatusList from '../../components/status/StatusList';
 
 const style = {
   container: css`
@@ -17,45 +18,64 @@ const style = {
   `,
 };
 
+interface StatusSummary {
+  id: number;
+  user: string;
+  title: string;
+  result: number;
+  time: string;
+  datetime: number;
+}
+
 function StatusDetail() {
   const router = useRouter();
   const { id } = router.query;
   const [code, setCode] = useState<string>('');
+  const [status, setStatus] = useState<StatusSummary | null>(null);
 
   useEffect(() => {
     (async () => {
       const res = await (await axios.get(`/api/submissions/${id}`)).data;
       setCode(res.code);
+      setStatus(res.status);
     })();
   }, []);
 
   return (
     <div css={style.container}>
       <div css={style.title}>풀이 상세</div>
-      <div
-        css={css`
-          height: 80%;
-          border: 1px solid rgba(0, 0, 0, 0.2);
-          padding: 15px 0;
-          border-radius: 1rem;
-        `}
-      >
-        <Editor
-          defaultLanguage="python"
-          defaultValue={code}
-          options={{
-            readOnly: true,
-            minimap: {
-              enabled: false,
-            },
-            scrollbar: {
-              vertical: 'hidden',
-              verticalScrollbarSize: 0,
-            },
-            scrollBeyondLastLine: false,
-          }}
-        />
-      </div>
+      {status === null ? (
+        <div>로딩중</div>
+      ) : (
+        <>
+          <StatusList status={status} />
+          <div
+            css={css`
+              height: 80%;
+              padding: 15px 0;
+              border-radius: 8px;
+              margin-top: 15px;
+              box-shadow: 0px 0px 1.5px 1.5px rgba(0, 0, 0, 0.15);
+            `}
+          >
+            <Editor
+              defaultLanguage="python"
+              defaultValue={code}
+              options={{
+                readOnly: true,
+                minimap: {
+                  enabled: false,
+                },
+                scrollbar: {
+                  vertical: 'hidden',
+                  verticalScrollbarSize: 0,
+                },
+                scrollBeyondLastLine: false,
+              }}
+            />
+          </div>
+        </>
+      )}
       <div
         css={css`
           display: flex;
