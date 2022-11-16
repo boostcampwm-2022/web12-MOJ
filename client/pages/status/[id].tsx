f024472807bb89862d1acce1184bb65619312fa4
joo-prog <1114nhj@naver.com>
2022-11-16 16:03:51 +0900

    feat: close #19, close #20
    코드 에디터에 사용자의 풀이를 띄우고 수정 불가하게 함
    
    Co-authored-by: dolphinlmg <dolphinlmg@users.noreply.github.com>

---
+++ client/pages/status/[id].tsx
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Button from '../../components/common/Button';
+import { css } from '@emotion/react';
+import Editor from '@monaco-editor/react';
+
+const style = {
+  container: css`
+    margin: 64px 20%;
+    height: 80vh;
+  `,
+  title: css`
+    font-size: 32px;
+    font-weight: bold;
+    margin: 42px 12px;
+  `,
+};
+
+function StatusDetail() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [code, setCode] = useState<string>('');
+
+  useEffect(() => {
+    (async () => {
+      const res = await (await axios.get(`/api/submissions/${id}`)).data;
+      setCode(res.code);
+    })();
+  }, []);
+
+  return (
+    <div css={style.container}>
+      <div css={style.title}>풀이 상세</div>
+      <div
+        css={css`
+          height: 80%;
+          border: 1px solid rgba(0, 0, 0, 0.2);
+          padding: 15px 0;
+          border-radius: 1rem;
+        `}
+      >
+        <Editor
+          defaultLanguage="python"
+          defaultValue={code}
+          options={{
+            readOnly: true,
+            minimap: {
+              enabled: false,
+            },
+            scrollbar: {
+              vertical: 'hidden',
+              verticalScrollbarSize: 0,
+            },
+            scrollBeyondLastLine: false,
+          }}
+        />
+      </div>
+      <div
+        css={css`
+          display: flex;
+          justify-content: flex-end;
+          padding: 10px 1px;
+          width: 100%;
+        `}
+      >
+        <Button>복사</Button>
+      </div>
+    </div>
+  );
+}
+
+export default StatusDetail;
