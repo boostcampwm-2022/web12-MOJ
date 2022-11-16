36d6b4b13285874e3b3fc4078d874dd1e3aa9dab
Hyoju Roh <63294532+joo-prog@users.noreply.github.com>
2022-11-16 18:43:32 +0900

    Feat/#11 (close #11) (#62)
    * feat: 풀이 상세 코드 mock api 생성
    
    Co-authored-by: dolphinlmg <dolphinlmg@users.noreply.github.com>
    
    * feat: close #19, close #20
    
    코드 에디터에 사용자의 풀이를 띄우고 수정 불가하게 함
    
    Co-authored-by: dolphinlmg <dolphinlmg@users.noreply.github.com>
    
    * feat: 풀이 상세 mock api status 추가
    
    Co-authored-by: dolphinlmg <dolphinlmg@users.noreply.github.com>
    
    * feat: close #17
    
    풀이 status list 추가
    
    Co-authored-by: dolphinlmg <dolphinlmg@users.noreply.github.com>
    
    Co-authored-by: dolphinlmg <dolphinlmg@users.noreply.github.com>

---
+++ client/pages/api/submissions/[id].ts
@@ -0,0 +1,57 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+function mockHandler(req: NextApiRequest, res: NextApiResponse) {
+  const mockData = {
+    status: {
+      id: 1,
+      user: 'hyoseok',
+      title: `알고리즘 망령 효석이`,
+      result: Math.floor(Math.random() * 4 + 1),
+      time: Math.floor(Math.random() * 150),
+      datetime: new Date().getTime(),
+    },
+    code: `import threading, zipfile
+import struct
+
+class AsyncZip(threading.Thread):
+    def __init__(self, infile, outfile):
+        threading.Thread.__init__(self)
+        self.infile = infile
+        self.outfile = outfile
+
+    def run(self):
+        f = zipfile.ZipFile(self.outfile, 'w', zipfile.ZIP_DEFLATED)
+        f.write(self.infile)
+        f.close()
+        print('Finished background zip of:', self.infile)
+
+background = AsyncZip('mydata.txt', 'myarchive.zip')
+background.start()
+print('The main program continues to run in foreground.')
+
+background.join()    # Wait for the background task to finish
+print('Main program waited until background was done.')
+
+with open('myfile.zip', 'rb') as f:
+    data = f.read()
+
+start = 0
+for i in range(3):                      # show the first 3 file headers
+    start += 14
+    fields = struct.unpack('<IIIHH', data[start:start+16])
+    crc32, comp_size, uncomp_size, filenamesize, extra_size = fields
+
+    start += 16
+    filename = data[start:start+filenamesize]
+    start += filenamesize
+    extra = data[start:start+extra_size]
+    print(filename, hex(crc32), comp_size, uncomp_size)
+
+    start += extra_size + comp_size     # skip to the next header
+`,
+  };
+
+  res.json(mockData);
+}
+
+export default mockHandler;
