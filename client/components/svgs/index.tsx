86ba055e0ba63b65afcd4371df3e6794a46fec8a
joo-prog <1114nhj@naver.com>
2022-11-17 15:47:03 +0900

    feat: close #27, close #38, close #39, close #40
    - close #23
    - 하단에 저장, 취소 버튼 추가
    - 추가버튼 클릭시 입출력 row 생성
    - custom hook 생성(useIOList())
    - 테케 1개 반드시 존재 확인(useEffect로 testcase 0개면 바로 테스트케이스 하나 추가해주는 방식)
    - 확인/취소 버튼 클릭시 출제 목록 페이지로 이동
    
    Co-authored-by: dolphinlmg <dolphinlmg@users.noreply.github.com>

--- client/components/svgs/index.tsx
+++ client/components/svgs/index.tsx
@@ -2,3 +2,4 @@ export { default as EditSvg } from './edit';
 export { default as DeleteSvg } from './delete';
 export { default as AddFileSvg } from './add-file';
 export { default as ToggleSvg } from './toggle';
+export { default as RemoveRowSvg } from './removeRow';
