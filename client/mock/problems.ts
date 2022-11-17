3645d5a128b66090b67ca990de6b929376341820
HD152513 <qag331@naver.com>
2022-11-17 14:28:27 +0900

    feat: 문제 공개 여부 Mock API 생성
    Co-authored-by: hyoseok <janghs0604@gmail.com>

--- client/mock/problems.ts
+++ client/mock/problems.ts
@@ -4,7 +4,7 @@ const problems = Array.from({ length: 113 }, (_, i) => {
     title: `${i + 1}번째 문제 - 알고리즘 망령 효석이`,
     rate: Math.floor(Math.random() * 100) + Math.floor(Math.random() * 10) / 10,
     datetime: new Date().getTime(),
-    state: Math.floor(Math.random() * 2),
+    visible: Math.floor(Math.random() * 2) === 0 ? false : true,
   };
 }).reverse();
 
