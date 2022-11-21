57b37c57e163d058c5860fd63c5bf68822121943
hyoseok0604 <janghs0604@gmail.com>
2022-11-21 15:38:00 +0900

    feat: typeorm module 설정
    Co-authored-by: dolphinlmg <dolphinlmg@users.noreply.github.com>
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRESQL_HOST'),
+        port: +configService.get('POSTGRESQL_PORT'),
+        username: configService.get('POSTGRESQL_USERNAME'),
+        password: configService.get('POSTGRESQL_PASSWORD'),
+        database: configService.get('POSTGRESQL_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
