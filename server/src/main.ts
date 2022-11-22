16ca1d4c72572a1c263f24bc66b8ad186a0161f5
dolphinlmg <dolphinlmg@naver.com>
2022-11-22 21:52:16 +0900

    fix: session cookie maxAge 설정
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,6 +15,9 @@ async function bootstrap() {
       secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 6, // 6 hours
+      },
     }),
   );
   await app.listen(4000);
