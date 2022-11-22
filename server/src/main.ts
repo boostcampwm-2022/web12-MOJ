a7c0a881ebbf1093f672f68c59ba82c5e4ac9d0c
dolphinlmg <dolphinlmg@naver.com>
2022-11-22 14:54:26 +0900

    feat: BE github login 구현
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   await app.listen(4000);
 }
 bootstrap();
---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, HttpStatus, Query, Req, Res } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Response, Request } from 'express';
+import { User } from './entities/user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('oauth')
+  async getOauthRedirect(
+    @Query('code') code: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const user: User = await this.usersService.getOauthRedirect(code);
+    const session: any = req.session;
+
+    session.userId = user.id;
+    session.userName = user.name;
+
+    res.status(HttpStatus.OK).end();
+  }
+}
