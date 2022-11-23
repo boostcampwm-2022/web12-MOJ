f48372dc2b4a094be23e7c9f4be0354776e7345f
Minhui Park <qag331@naver.com>
2022-11-23 16:52:21 +0900

    9. POST /problems/:id/tc (close #90 close #111) (#112)
    * fix: 암시적 형변환 제거
    
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>
    
    * feat: Close #90
    
    - POST /api/problems/:id/tc
      - 예외처리
    
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>
    
    * feat: FE API 연결
    
    - /my-problem/tc/:id
      - 에러 메시지 수정
      - POST /api/problems/:id/tc
    
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>
    
    Co-authored-by: dolphinlmg <dolphinlmg@naver.com>
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>

--- server/src/problems/problems.service.ts
+++ server/src/problems/problems.service.ts
@@ -8,7 +8,7 @@ import {
 import { InjectRepository, InjectDataSource } from '@nestjs/typeorm';
 import { Repository, DataSource, EntityManager } from 'typeorm';
 import { CreateProblemDTO } from './dtos/create-problem.dto';
-import { GetTestCaseDTO } from './dtos/get-testcase.dto';
+import { PostTestCaseDTO } from './dtos/post-testcase.dto';
 import { Example } from './entities/example.entity';
 import { Problem } from './entities/problem.entity';
 import { Testcase } from './entities/testcase.entity';
@@ -44,15 +44,15 @@ export class ProblemsService {
     );
   }
 
-  async getTestCase(getTestCaseDTO: GetTestCaseDTO, userId: number) {
+  async getTestCase(id: number, userId: number) {
     const [testCases, problem] = await Promise.all([
       this.testcaseRepository.find({
         select: { id: true, input: true, output: true },
-        where: { problemId: getTestCaseDTO.id },
+        where: { problemId: id },
       }),
       this.problemRepository.findOne({
         select: { title: true, userId: true },
-        where: { id: getTestCaseDTO.id },
+        where: { id },
       }),
     ]);
 
@@ -87,5 +87,42 @@ export class ProblemsService {
     });
 
     return { ...problem, examples: example };
+  }
+
+  async postTestcase(
+    userId: number,
+    problemId: number,
+    postTestCaseDTO: PostTestCaseDTO,
+  ) {
+    const problem = await this.problemRepository.findOneBy({ id: problemId });
+
+    if (!problem) throw new NotFoundException('해당 문제가 없습니다.');
+
+    if (!userId || problem.userId !== userId) {
+      throw new ForbiddenException('권한이 없습니다.');
+    }
+
+    await this.dataSource.manager.transaction(
+      async (transactionEntityManager: EntityManager) => {
+        await transactionEntityManager
+          .createQueryBuilder()
+          .delete()
+          .from(Testcase)
+          .where('problemId = :id', { id: problemId })
+          .execute();
+
+        await transactionEntityManager
+          .createQueryBuilder()
+          .insert()
+          .into(Testcase)
+          .values(
+            [...postTestCaseDTO.testcase].map((tc) => ({
+              ...tc,
+              problemId: problemId,
+            })),
+          )
+          .execute();
+      },
+    );
   }
 }
