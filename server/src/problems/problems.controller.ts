177a153250d66b35d6729c6e8947a02c313c244a
Hyoju Roh <63294532+joo-prog@users.noreply.github.com>
2022-11-23 14:40:33 +0900

    5. GET /api/problems/:id (close #83) (#105)
    * feat: 5. GET /api/problems/:id (close #83
    
    Co-authored-by: hyoseok <janghs0604@gmail.com>
    
    * feat: #83 API 프론트 연결 (close #101)
    
    - Editor보다 문제가 빨리 로딩된 경우와 문제보다 Editor가 빨리 로딩된 경우 둘다 고려
    - 프로퍼티명 API에 맞게 수정
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * fix: problem 테이블 빠진 컬럼 추가
    
    - userId 컬럼 : 출제자 아이디
    - limit 컬럼 : 제한 설명
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * feat: 403, 404 예외 처리
    
    - 404 : 문제를 찾을 수 없는 경우
    - 403 : 비공개 문제를 출제자가 아닌 사람 또는 비로그인 사용자가 접근한 경우
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    * feat: 에러 발생 시 뒤로가기
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    
    Co-authored-by: hyoseok <janghs0604@gmail.com>
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>

--- server/src/problems/problems.controller.ts
+++ server/src/problems/problems.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { CreateProblemDTO } from './dtos/create-problem.dto';
 import { ProblemsService } from './problems.service';
+import { Request } from 'express';
 
 @Controller('problems')
 export class ProblemsController {
@@ -8,7 +18,27 @@ export class ProblemsController {
 
   @Post()
   async create(@Body() createProblemDTO: CreateProblemDTO) {
-    console.log(createProblemDTO);
     return this.problemsService.create(createProblemDTO);
+  }
+
+  @Get('/:id')
+  async findOne(
+    @Req() req: Request,
+    @Param(
+      'id',
+      new ParseIntPipe({
+        exceptionFactory: () =>
+          new BadRequestException('문제 번호가 숫자가 아닙니다.'),
+      }),
+    )
+    id: number,
+  ) {
+    const session: any = req.session;
+
+    if (!!session.userId && !!session.userName) {
+      return this.problemsService.findOne(id, session.userId);
+    } else {
+      return this.problemsService.findOne(id, false);
+    }
   }
 }
