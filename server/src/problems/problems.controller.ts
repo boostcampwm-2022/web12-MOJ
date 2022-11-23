7c0ae407e9e6977ecb4e7436f8af227ef5e09eb7
hyoseok0604 <janghs0604@gmail.com>
2022-11-23 12:02:04 +0900

    feat: 403, 404 예외 처리
    - 404 : 문제를 찾을 수 없는 경우
    - 403 : 비공개 문제를 출제자가 아닌 사람 또는 비로그인 사용자가 접근한 경우
    
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>

--- server/src/problems/problems.controller.ts
+++ server/src/problems/problems.controller.ts
@@ -6,10 +6,11 @@ import {
   Param,
   ParseIntPipe,
   Post,
-  Query,
+  Req,
 } from '@nestjs/common';
 import { CreateProblemDTO } from './dtos/create-problem.dto';
 import { ProblemsService } from './problems.service';
+import { Request } from 'express';
 
 @Controller('problems')
 export class ProblemsController {
@@ -22,16 +23,22 @@ export class ProblemsController {
 
   @Get('/:id')
   async findOne(
+    @Req() req: Request,
     @Param(
       'id',
       new ParseIntPipe({
         exceptionFactory: () =>
-          new BadRequestException('문제 번호 형식이 숫자가 아닙니다.'),
+          new BadRequestException('문제 번호가 숫자가 아닙니다.'),
       }),
     )
     id: number,
   ) {
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
-    return this.problemsService.findOne(id);
+    const session: any = req.session;
+
+    if (!!session.userId && !!session.userName) {
+      return this.problemsService.findOne(id, session.userId);
+    } else {
+      return this.problemsService.findOne(id, false);
+    }
   }
 }
--- server/src/problems/problems.service.ts
+++ server/src/problems/problems.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository, InjectDataSource } from '@nestjs/typeorm';
 import { Repository, DataSource, EntityManager } from 'typeorm';
 import { CreateProblemDTO } from './dtos/create-problem.dto';
@@ -31,13 +35,22 @@ export class ProblemsService {
     );
   }
 
-  async findOne(id: number) {
+  async findOne(id: number, userId: number | false) {
     const problem = await this.problemRepository.findOneBy({ id });
+
+    if (!problem) throw new NotFoundException('해당 문제가 없습니다.');
+
+    if (!problem.visible) {
+      if (!userId || problem.userId !== userId) {
+        throw new ForbiddenException('권한이 없습니다.');
+      }
+    }
+
     const example = await this.exampleRepository.find({
       select: ['id', 'input', 'output'],
       where: { problemId: id },
     });
-    console.log(problem, example);
+
     return { ...problem, examples: example };
   }
 }
