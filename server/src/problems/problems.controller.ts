6d2b8d80bc72aaf791134bfe741a700e67a67aad
dolphinlmg <dolphinlmg@naver.com>
2022-11-23 14:01:19 +0900

    feat: GET /api/problems/:id/tc 구현
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>

--- server/src/problems/problems.controller.ts
+++ server/src/problems/problems.controller.ts
@@ -1,5 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { CreateProblemDTO } from './dtos/create-problem.dto';
+import { GetTestCaseDTO } from './dtos/get-testcase.dto';
 import { ProblemsService } from './problems.service';
 
 @Controller('problems')
@@ -10,5 +21,22 @@ export class ProblemsController {
   async create(@Body() createProblemDTO: CreateProblemDTO) {
     console.log(createProblemDTO);
     return this.problemsService.create(createProblemDTO);
+  }
+
+  @Get(':id/tc')
+  async getTestcase(
+    @Req() req: Request,
+    @Param() getTestCaseDTO: GetTestCaseDTO,
+  ) {
+    const session: any = req.session;
+
+    if (!session.userId) {
+      throw new HttpException(
+        '로그인이 되어있지 않습니다.',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    return this.problemsService.getTestCase(getTestCaseDTO, session.userId);
   }
 }
