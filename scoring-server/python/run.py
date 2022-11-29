792aa83ae656a4d82833999003f6be7c23db2384
dolphinlmg <dolphinlmg@naver.com>
2022-11-29 15:02:00 +0900

    fix: Mock python 채점 코드 수정
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>

--- scoring-server/python/run.py
+++ scoring-server/python/run.py
@@ -1,8 +1,13 @@
 import sys
+import time
+import random
 
 def run(code):
-    print(code)
+    for _ in range(5):
+        time.sleep(1)
+    return random.randrange(0,7)
+
 
 if __name__ == '__main__':
     if (len(sys.argv) > 1):
-        run(sys.argv[1])
+        print(run(sys.argv[1]))
