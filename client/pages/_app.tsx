34d3dc901672331ab70e514df30c9e6f2773676d
dolphinlmg <dolphinlmg@naver.com>
2022-12-05 16:07:54 +0900

    feat: title 추가
    Co-authored-by: Hyoju Roh <joo-prog@users.noreply.github.com>
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { Global, css } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import GNB from '../components/GNB';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>MOJ</title>
+      </Head>
       <Global
         styles={css`
           body,
