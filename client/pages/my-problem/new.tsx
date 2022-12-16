59a50c408f49a76a877635998db5bf8949010fdb
joo-prog <1114nhj@naver.com>
2022-12-16 10:51:03 +0900

    fix: 오류 메시지 수정(#208)
    Co-authored-by: Minhui Park <Minhee331@users.noreply.github.com>
    Co-authored-by: hyoseok <janghs0604@gmail.com>
    Co-authored-by: dolphinlmg <dolphinlmg@users.noreply.github.com>

--- client/pages/my-problem/new.tsx
+++ client/pages/my-problem/new.tsx
@@ -70,8 +70,7 @@ function NewMyProblem() {
       }
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        if (error.response?.status === 400)
-          alert('입력 형식 및 빈 칸이 없는지 확인하세요.');
+        if (error.response?.status === 400) alert(error.response.data.message);
         if (error.response?.status === 401) {
           alert('로그인이 필요한 서비스입니다.');
           Router.back();
